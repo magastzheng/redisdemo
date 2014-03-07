f51c7fb1671e455520997bff6360e2317ca253d2
Magast Zheng <magast.zheng@morningstar.com>
2014-03-07 16:05:13 +0800

    Initial the repo to add code

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RedisClient("127.0.0.1", 6379);
+            //var s = client.AddItemToSortedSet("12", "baidu", 400);
+            //client.AddItemToSortedSet("12", "google", 300);
+            //client.AddItemToSortedSet("12", "alibaba", 200);
+            //client.AddItemToSortedSet("12", "sina", 100);
+            //client.AddItemToSortedSet("12", "renren", 500);
+            //client.AddItemToSortedSet("12", "tencent", 150);
+
+            var list = client.GetRangeFromSortedSet("12", 0, 0);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //descending
+            list = client.GetRangeFromSortedSetDesc("12", 0, 0);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
